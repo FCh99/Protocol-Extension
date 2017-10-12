7a5ccfc70ed2bd76304e04ed8bce5c3c42ff37dc
Fausto Checa <fausto.checa@gmail.com>
2017-10-13 00:21:47 +0200

    IOS

--- MyApp/ViewController.swift
+++ MyApp/ViewController.swift
@@ -8,18 +8,101 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+protocol Viajar {
+    var diaSalida : DiasSemana {get set}
+    var destino : String {get set}
+    func hacerEquipaje ()
+    func viajar ()
+    func llegar ()
+}
 
+extension Viajar {
+    func tomaFotos () -> Void{
+        print("Clck, click, click")
+        
+    }
+    
+}
+
+enum DiasSemana {
+    case Lunes, Martes, Miercoles, Jueves, Viernes, Sabado, Domingo
+    
+}
+
+class Verano : Viajar {
+    
+    var diaSalida: DiasSemana = .Lunes
+    var destino: String = ""
+    
+    func hacerEquipaje() {
+        print ("Haciendo el euqipaje")
+    }
+    
+    func viajar() {
+        print ("viajando...")
+    }
+    
+    func llegar() {
+        print ("abre la puerta ¡¡¡")
+    }
+    
+    init(dia: DiasSemana, destino: String) {
+        self.diaSalida = dia
+        self.destino = destino
+    }
+}
+
+class SemanaSanta : Viajar {
+    
+    var diaSalida: DiasSemana
+    var destino: String = ""
+    
+    func hacerEquipaje() {
+        print("llenando la maleta...")
+    }
+    
+    func viajar() {
+        print("run run")
+    }
+    
+    func llegar() {
+        print("LLama a casa que ya hemos llegado")
+    }
+    
+    init(dia:DiasSemana, destino:String) {
+        self.diaSalida = dia
+        self.destino = destino
+    }
+    
+}
+
+
+
+class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let misQuinceDias = Verano(dia: .Domingo, destino: "Chiclana")
+        
+        print(misQuinceDias.diaSalida)
+        print(misQuinceDias.destino)
+        misQuinceDias.hacerEquipaje()
+        misQuinceDias.viajar()
+        misQuinceDias.llegar()
+        misQuinceDias.tomaFotos()
+        
+        print("--------------")
+        
+        let miSemanita = SemanaSanta(dia: .Jueves, destino: "Malaga")
+        print(miSemanita.diaSalida)
+        print(miSemanita.destino)
+        miSemanita.hacerEquipaje()
+        miSemanita.viajar()
+        miSemanita.tomaFotos()
+        miSemanita.llegar()
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
+
 
